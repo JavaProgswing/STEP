8939c74f39f0c9e85877e603bcca081612f09939
TJP <69581225+JavaProgswing@users.noreply.github.com>
2026-03-24 08:41:17 +0530

    feat: Implement UC2 - Basic Room Types & Static Availability

--- src/HotelBookingApp.java
+++ src/HotelBookingApp.java
@@ -1,6 +1,84 @@
+/**
+ * Use Case 2: Basic Room Types & Static Availability
+ * Book My Stay App - Version 2.1
+ */
+
+abstract class Room {
+
+    protected String roomType;
+    protected int beds;
+    protected double price;
+
+    public Room(String roomType, int beds, double price) {
+        this.roomType = roomType;
+        this.beds = beds;
+        this.price = price;
+    }
+
+    public abstract void displayRoomDetails();
+}
+
+class SingleRoom extends Room {
+
+    public SingleRoom() {
+        super("Single Room", 1, 2000);
+    }
+
+    public void displayRoomDetails() {
+        System.out.println(roomType + ": ");
+        System.out.println("Beds: " + beds);
+        System.out.println("Price per night: " + price);
+    }
+}
+
+class DoubleRoom extends Room {
+
+    public DoubleRoom() {
+        super("Double Room", 2, 3500);
+    }
+
+    public void displayRoomDetails() {
+        System.out.println(roomType + ": ");
+        System.out.println("Beds: " + beds);
+        System.out.println("Price per night: ₹" + price);
+    }
+}
+
+class SuiteRoom extends Room {
+
+    public SuiteRoom() {
+        super("Suite Room", 3, 6000);
+    }
+
+    public void displayRoomDetails() {
+        System.out.println(roomType + ": ");
+        System.out.println("Beds: " + beds);
+        System.out.println("Price per night: " + price);
+    }
+}
+
 public class HotelBookingApp {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Hotel Booking Management System");
-        System.out.println("System initialized successfully.");
+        System.out.println("Hotel Room Initialization");
+        System.out.println();
+
+        Room single = new SingleRoom();
+        Room doub = new DoubleRoom();
+        Room suite = new SuiteRoom();
+
+        int singleAvailability = 10;
+        int doubleAvailability = 5;
+        int suiteAvailability = 2;
+
+        single.displayRoomDetails();
+        System.out.println("Available: " + singleAvailability);
+        System.out.println();
+
+        doub.displayRoomDetails();
+        System.out.println("Available: " + doubleAvailability);
+        System.out.println();
+
+        suite.displayRoomDetails();
+        System.out.println("Available: " + suiteAvailability);
     }
 }
