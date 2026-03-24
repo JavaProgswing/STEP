84419033aab62bff0f93d0fc72f2cc17f0db50b7
TJP <69581225+JavaProgswing@users.noreply.github.com>
2026-03-24 09:19:05 +0530

    feat: Implement UC5 - Preserve Insertion Order of Bogies (LinkedHashSet)

--- src/TrainConsistMgmnt.java
+++ src/TrainConsistMgmnt.java
@@ -1,47 +1,24 @@
-import java.util.HashSet;
-import java.util.LinkedList;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 public class TrainConsistMgmnt {
 
     public static void main(String[] args) {
 
         System.out.println("======================================");
-        System.out.println(" UC4 - Maintain Ordered Bogie Consist ");
+        System.out.println(" UC5 - Preserve Insertion Order of Bogies ");
         System.out.println("======================================\n");
-        LinkedList<String> trainConsist = new LinkedList<>();
-        HashSet<String> uniqueCheck = new HashSet<>();
-        addBogie(trainConsist, uniqueCheck, "Engine");
-        addBogie(trainConsist, uniqueCheck, "Sleeper");
-        addBogie(trainConsist, uniqueCheck, "AC");
-        addBogie(trainConsist, uniqueCheck, "Cargo");
-        addBogie(trainConsist, uniqueCheck, "Guard");
+        Set<String> formation = new LinkedHashSet<>();
+        formation.add("Engine");
+        formation.add("Sleeper");
+        formation.add("Cargo");
+        formation.add("Guard");
+        formation.add("Sleeper"); // ignored
+        System.out.println("Final Train Formation:");
+        System.out.println(formation);
+        System.out.println("\nNote:");
+        System.out.println("LinkedHashSet preserves insertion order and removes duplicates automatically.");
 
-        System.out.println("Initial Train Consist:");
-        display(trainConsist);
-        String pantry = "Pantry";
-        if (!uniqueCheck.contains(pantry)) {
-            trainConsist.add(1, pantry);
-            uniqueCheck.add(pantry);
-        }
-
-        System.out.println("\nAfter inserting Pantry at position 2:");
-        display(trainConsist);
-        trainConsist.removeFirst(); // removes Engine
-        trainConsist.removeLast();  // removes Guard
-
-        System.out.println("\nAfter removing first and last bogie:");
-        display(trainConsist);
-    }
-
-    private static void addBogie(LinkedList<String> list, HashSet<String> set, String bogie) {
-        if (set.add(bogie)) {
-            list.addLast(bogie);
-        } else {
-            System.out.println("Duplicate ignored: " + bogie);
-        }
-    }
-
-    private static void display(LinkedList<String> list) {
-        System.out.println(String.join(" -> ", list));
+        System.out.println("\nUC5 formation setup completed...");
     }
 }
