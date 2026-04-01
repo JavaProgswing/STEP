dbd3b4b6b39b7c4647a3ed752e2298bdd9aff060
TJP <69581225+JavaProgswing@users.noreply.github.com>
2026-04-01 09:42:02 +0530

    feat: implement UC13 - Performance Comparison (Loops vs Streams)

--- src/TrainConsistMgmnt.java
+++ src/TrainConsistMgmnt.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Bogie {
     private String name;
     private int capacity;
@@ -21,13 +24,27 @@ class Bogie {
     }
 }
 
-
 public class TrainConsistMgmnt {
-    public static boolean checkSafetyCompliance(String cargoType) {
-        return cargoType.equals("Flammable") || cargoType.equals("Non-Flammable");
+    public static void performanceComparison(List<Bogie> bogies) {
+        long startTime = System.nanoTime();
+        for (Bogie bogie : bogies) {
+            System.out.println(bogie);
+        }
+        long endTime = System.nanoTime();
+        System.out.println("Loop Time: " + (endTime - startTime) + " ns");
+
+        startTime = System.nanoTime();
+        bogies.stream().forEach(System.out::println);
+        endTime = System.nanoTime();
+        System.out.println("Stream Time: " + (endTime - startTime) + " ns");
     }
 
     public static void main(String[] args) {
-        System.out.println("Is safety compliant: " + checkSafetyCompliance("Flammable"));
+        List<Bogie> bogies = new ArrayList<>();
+        bogies.add(new Bogie("Sleeper", 72));
+        bogies.add(new Bogie("AC Chair", 56));
+        bogies.add(new Bogie("First Class", 30));
+
+        performanceComparison(bogies);
     }
 }
