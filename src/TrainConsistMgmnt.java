02ba97d928e942d8e1c590c85a71df4b5c32738e
TJP <69581225+JavaProgswing@users.noreply.github.com>
2026-04-09 09:03:07 +0530

    feat: implement UC17 - Sort Bogie Names Using Arrays.sort()

--- src/TrainConsistMgmnt.java
+++ src/TrainConsistMgmnt.java
@@ -26,27 +26,15 @@ class Bogie {
 
 
 public class TrainConsistMgmnt {
-    public static void bubbleSort(List<Bogie> bogies) {
-        int n = bogies.size();
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-                if (bogies.get(j).getCapacity() < bogies.get(j + 1).getCapacity()) {
-                    Bogie temp = bogies.get(j);
-                    bogies.set(j, bogies.get(j + 1));
-                    bogies.set(j + 1, temp);
-                }
-            }
-        }
-    }
-
     public static void main(String[] args) {
         List<Bogie> bogies = new ArrayList<>();
         bogies.add(new Bogie("Sleeper", 72));
         bogies.add(new Bogie("AC Chair", 56));
         bogies.add(new Bogie("First Class", 30));
 
-        bubbleSort(bogies);
-        System.out.println("Sorted Bogies by Capacity:");
-        bogies.forEach(System.out::println);
+        String[] names = bogies.stream().map(Bogie::getName).toArray(String[]::new);
+        java.util.Arrays.sort(names);
+        System.out.println("Sorted Bogies by Name:");
+        System.out.println(String.join(", ", names));
     }
 }
