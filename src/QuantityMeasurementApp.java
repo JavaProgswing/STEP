d205cd6f47a918101078ebcc22d358b751ceb83b
TJP <69581225+JavaProgswing@users.noreply.github.com>
2026-04-27 19:55:17 +0530

    feat: implement UC2: Feet and Inches measurement equality

--- src/QuantityMeasurementApp.java
+++ src/QuantityMeasurementApp.java
@@ -7,10 +7,6 @@ public class QuantityMeasurementApp {
             this.value = value;
         }
 
-        public double getValue() {
-            return value;
-        }
-
         @Override
         public boolean equals(Object obj) {
             if (this == obj) return true;
@@ -20,10 +16,32 @@ public class QuantityMeasurementApp {
         }
     }
 
-    public static void main(String[] args) {
-        Feet f1 = new Feet(1.0);
-        Feet f2 = new Feet(1.0);
+    public static class Inches {
+        private final double value;
 
-        System.out.println(f1.equals(f2));
+        public Inches(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null || getClass() != obj.getClass()) return false;
+            Inches other = (Inches) obj;
+            return Double.compare(this.value, other.value) == 0;
+        }
+    }
+
+    public static boolean compareFeet(double v1, double v2) {
+        return new Feet(v1).equals(new Feet(v2));
+    }
+
+    public static boolean compareInches(double v1, double v2) {
+        return new Inches(v1).equals(new Inches(v2));
+    }
+
+    public static void main(String[] args) {
+        System.out.println(compareFeet(1.0, 1.0));
+        System.out.println(compareInches(1.0, 1.0));
     }
 }
