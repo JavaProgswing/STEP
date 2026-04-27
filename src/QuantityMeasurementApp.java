d2c3828a706857cef5fcd50f42bd405b838a41b6
TJP <69581225+JavaProgswing@users.noreply.github.com>
2026-04-27 19:43:07 +0530

    feat: implement UC1: Feet measurement equality

--- src/QuantityMeasurementApp.java
+++ src/QuantityMeasurementApp.java
@@ -1,26 +1,29 @@
 public class QuantityMeasurementApp {
-    public static void main(String[] args) {
-        // Demonstration of UC7 examples
-        printExample(new QuantityLength(1.0, LengthUnit.FEET), new QuantityLength(12.0, LengthUnit.INCHES), LengthUnit.FEET);
-        printExample(new QuantityLength(1.0, LengthUnit.FEET), new QuantityLength(12.0, LengthUnit.INCHES), LengthUnit.INCHES);
-        printExample(new QuantityLength(1.0, LengthUnit.FEET), new QuantityLength(12.0, LengthUnit.INCHES), LengthUnit.YARDS);
-        printExample(new QuantityLength(1.0, LengthUnit.YARDS), new QuantityLength(3.0, LengthUnit.FEET), LengthUnit.YARDS);
-        printExample(new QuantityLength(36.0, LengthUnit.INCHES), new QuantityLength(1.0, LengthUnit.YARDS), LengthUnit.FEET);
-        printExample(new QuantityLength(2.54, LengthUnit.CENTIMETERS), new QuantityLength(1.0, LengthUnit.INCHES), LengthUnit.CENTIMETERS);
-        printExample(new QuantityLength(5.0, LengthUnit.FEET), new QuantityLength(0.0, LengthUnit.INCHES), LengthUnit.YARDS);
-        printExample(new QuantityLength(5.0, LengthUnit.FEET), new QuantityLength(-2.0, LengthUnit.FEET), LengthUnit.INCHES);
 
-        // Edge: null target unit should throw
-        try {
-            QuantityLength.add(new QuantityLength(1.0, LengthUnit.FEET), new QuantityLength(12.0, LengthUnit.INCHES), null);
-        } catch (IllegalArgumentException e) {
-            System.out.println("Expected exception for null target unit: " + e.getMessage());
+    public static class Feet {
+        private final double value;
+
+        public Feet(double value) {
+            this.value = value;
+        }
+
+        public double getValue() {
+            return value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null || getClass() != obj.getClass()) return false;
+            Feet other = (Feet) obj;
+            return Double.compare(this.value, other.value) == 0;
         }
     }
 
-    private static void printExample(QuantityLength a, QuantityLength b, LengthUnit target) {
-        QuantityLength result = QuantityLength.add(a, b, target);
-        System.out.println("Input: add(" + a + ", " + b + ", " + target + ") -> Output: " + result);
+    public static void main(String[] args) {
+        Feet f1 = new Feet(1.0);
+        Feet f2 = new Feet(1.0);
+
+        System.out.println(f1.equals(f2));
     }
 }
-
