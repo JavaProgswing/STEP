6c2ca58a9f12491bee0cfbacfa9ef58ee7599bc3
TJP <69581225+JavaProgswing@users.noreply.github.com>
2026-04-27 20:21:24 +0530

    feat: implement UC11: Volume Measurement Equality, Conversion, and Addition (Litre, Millilitre, Gallon)

--- src/QuantityMeasurementApp.java
+++ src/QuantityMeasurementApp.java
@@ -14,6 +14,7 @@ public class QuantityMeasurementApp {
 
     public static void main(String[] args) {
 
+        // Length
         Quantity<LengthUnit> l1 = new Quantity<>(1.0, LengthUnit.FEET);
         Quantity<LengthUnit> l2 = new Quantity<>(12.0, LengthUnit.INCHES);
 
@@ -21,11 +22,25 @@ public class QuantityMeasurementApp {
         demonstrateConversion(l1, LengthUnit.INCHES);
         demonstrateAddition(l1, l2, LengthUnit.FEET);
 
+        // Weight
         Quantity<WeightUnit> w1 = new Quantity<>(1.0, WeightUnit.KILOGRAM);
         Quantity<WeightUnit> w2 = new Quantity<>(1000.0, WeightUnit.GRAM);
 
         demonstrateEquality(w1, w2);
         demonstrateConversion(w1, WeightUnit.GRAM);
         demonstrateAddition(w1, w2, WeightUnit.KILOGRAM);
+
+        // Volume (UC11)
+        Quantity<VolumeUnit> v1 = new Quantity<>(1.0, VolumeUnit.LITRE);
+        Quantity<VolumeUnit> v2 = new Quantity<>(1000.0, VolumeUnit.MILLILITRE);
+        Quantity<VolumeUnit> v3 = new Quantity<>(1.0, VolumeUnit.GALLON);
+
+        demonstrateEquality(v1, v2);
+        demonstrateConversion(v1, VolumeUnit.MILLILITRE);
+        demonstrateAddition(v1, v2, VolumeUnit.LITRE);
+
+        demonstrateEquality(v1, v3);
+        demonstrateConversion(v3, VolumeUnit.LITRE);
+        demonstrateAddition(v1, v3, VolumeUnit.MILLILITRE);
     }
 }
